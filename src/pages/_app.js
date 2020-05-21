31daac1509e59e771282e723a1b1fe14b39c5e2c
Marcelo Nascimento <marcelo.menezes@gmail.com>
2020-05-21 18:24:19 -0300

    Update deps and move Head component to layout file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,18 @@
 import React from "react";
+import Head from "next/head";
 import "../css/tailwind.css";
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title className="m">Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default App;
